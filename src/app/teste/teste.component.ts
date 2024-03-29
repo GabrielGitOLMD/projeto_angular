7354faba9d33167f2d8c4e04b8f1580e25cd8375
gabriel santana <neivagabriel948@gmail.com>
2024-03-29 00:34:08 -0300

    angular

---
+++ src/app/teste/teste.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teste',
+ 
+standalone:true,
+  templateUrl: './teste.component.html',
+  styleUrl: './teste.component.css'
+})
+export class TesteComponent {
+
+}
